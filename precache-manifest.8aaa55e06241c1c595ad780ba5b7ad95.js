db4565c1fa68a00d0eb56732af31f0326cc6bb4c
moha123456987 <61421685+moha123456987@users.noreply.github.com>
2020-09-23 12:44:37 +0200

    Updates

---
+++ precache-manifest.8aaa55e06241c1c595ad780ba5b7ad95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b53870b02989e5a31257a0802bc35533",
+    "url": "/Employee-Manager/index.html"
+  },
+  {
+    "revision": "b038ac2b16b4926c94e7",
+    "url": "/Employee-Manager/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b038ac2b16b4926c94e7",
+    "url": "/Employee-Manager/static/js/2.d64b1374.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/Employee-Manager/static/js/2.d64b1374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75c1fdff258a1410ecf0",
+    "url": "/Employee-Manager/static/js/main.6ff80e98.chunk.js"
+  },
+  {
+    "revision": "b108b9ae486058f39d8f",
+    "url": "/Employee-Manager/static/js/runtime-main.58e160c6.js"
+  }
+]);
